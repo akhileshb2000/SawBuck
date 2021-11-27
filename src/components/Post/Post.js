8792e262dcb85e8821bd0d8353edb5be88deef37
Rohan Kalra <rohankal@usc.edu>
2021-11-27 15:42:38 -0800

    rendering a video that repeats

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,24 +1,42 @@
-import React from "react";
+import React, {useState} from "react";
 import { 
     View, 
-    Text
+    Text,
+    Button,
+    TouchableWithoutFeedback
  } from "react-native";
 
-import Video from "react-native-video";
+import { Video, AVPlaybackStatus } from 'expo-av';
+//import Video from "expo"
+
+// import Video from "react-native-video";
 import styles from "./PostStyles";
 
 const Post = () => {
+
+     const [paused, setPaused] =  useState(false);
+
+
+    const onPlayPausePress = () => {
+        console.warn("touched!");
+        setPaused(!paused);
+    }
+ 
     return (
-        <View style={styles.container}>
-            <Text>
-                This is a post.
-            </Text>
-            {/* <Video
-                source={ { uri: "../../assets/videos/rr.mp4"} }
+      <View style={styles.container}>
+        <TouchableWithoutFeedback onPress={onPlayPausePress}>
+            <Video
                 style={styles.video}
-            /> */}
-        </View>
-    )
+                source={ require("../../assets/videos/test.mp4") }
+                shouldPlay={true}
+                resizeMode="contain"
+                isLooping={true}
+                paused={paused}
+            />
+        </TouchableWithoutFeedback> 
+
+      </View>
+    );
 }
 
 export default Post;
