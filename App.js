8fd60f8327de37910e42bdb7fdb68669482a5dd9
Rohan Kalra <rohankal@usc.edu>
2021-11-26 17:44:07 -0800

    test

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>LavaLab F21</Text>
+      <Text>LavaLab F21!</Text>
       <StatusBar style="auto" />
     </View>
   );
