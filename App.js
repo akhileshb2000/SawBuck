b3bc05cdc64996e6d8a71f6bc09f7e1edc4d11ac
Rohan Kalra <rohankal@usc.edu>
2021-11-27 16:58:23 -0800

    got rid of safe view

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Text, SafeAreaView } from 'react-native';
+import { SafeAreaView, Text, View } from 'react-native';
 
 import Feed from './src/screens/Feed/Feed';
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <Feed></Feed>
-    </SafeAreaView>
+    <>
+      <View> 
+        <Feed></Feed> 
+      </View>
+
+    </>
   );
 }
