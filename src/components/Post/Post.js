a8a2a113166c002daea5428a781a7679f5262b4d
Rohan Kalra <rohankal@usc.edu>
2021-12-01 20:03:02 -0800

    fixed video rendering to fit entire screen

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,17 +1,15 @@
 import React, {useState} from "react";
 import { 
     View, 
-    Text,
-    Button,
     TouchableWithoutFeedback
  } from "react-native";
 
 import { Video } from 'expo-av';
 import styles from "./PostStyles";
-
+import api from "../../services/api";
 import { AntDesign } from '@expo/vector-icons';
 
-const Post = () => {
+const Post = ({index}) => {
 
     const video = React.useRef(null);
     const [status, setStatus] = React.useState({});
@@ -28,33 +26,13 @@ const Post = () => {
             <Video
                 ref={video}
                 style={styles.video}
-                source={ require("../../../assets/apartmentVid.mp4") }
+                source={ require('../../../assets/apartmentVid.mp4') }
                 shouldPlay={true}
-                resizeMode="contain"
+                resizeMode={"cover"}
                 isLooping={true}
                 onPlaybackStatusUpdate={status => setStatus(() => status)}
             />
         </TouchableWithoutFeedback>
-        {/* <View style={styles.uiContainer}>
-            <View style={styles.rightSide}>
-                <AntDesign name="right" size={24} color="#FFF" />
-            </View>
-            <View style={styles.bottomSide}>
-                <Text style={styles.rapperName}>
-                    @Roheezy
-                </Text>
-                <Text style={styles.hometown}>
-                    Leesburg, VA.
-                </Text>
-                <View style={styles.priceRow}>
-                    Need to do the Current Price + The Price Change
-                    <Text style={styles.currentPrice}>$RICH = $124.67</Text>
-                    <AntDesign name="caretup" size={24} color= "#00FFA3" />
-                    <Text style={styles.priceChange}>52% last month</Text>
-                </View>
-            </View>
-
-        </View> */}
       </View>
      
     );
